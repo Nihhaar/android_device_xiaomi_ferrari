f6869a7f9789ff838d1137f205a211b63120fac0
Nihhaar <nihhaar@gmail.com>
2017-03-22 03:53:19 +0000

    ferrari: adapt dt for omni

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,5 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_ferrari.mk
+    $(LOCAL_DIR)/full_ferrari.mk \
+    $(LOCAL_DIR)/omni_ferrari.mk
--- omni_ferrari.mk
+++ omni_ferrari.mk
@@ -16,14 +16,14 @@
 $(call inherit-product, device/xiaomi/ferrari/full_ferrari.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ferrari
-PRODUCT_NAME := lineage_ferrari
+PRODUCT_NAME := omni_ferrari
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 4i
 PRODUCT_MANUFACTURER := Xiaomi
